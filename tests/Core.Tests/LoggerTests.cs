d3f2f69240115452ebacc4b182155637a2a35d0f
Joshua Moon <joshua@moon.uk.net>
2015-08-20 22:11:34 +0100

    Updated basic wrapper tests or warning level

--- tests/Core.Tests/LoggerTests.cs
+++ tests/Core.Tests/LoggerTests.cs
@@ -1,66 +1,66 @@
-﻿//#region Header
-//// <copyright company="Joshua Moon" file="LoggerTests.cs">
-//// Copyright (c) 2015 Joshua Moon All Rights Reserved.
-//// </copyright>
-//// <summary>
-//// LoggerTests.cs is a part of the project Core.Tests. 
-//// </summary>
-//// <author>Joshua Moon</author>
-//// <license>
-//// This software may be modified and distributed under the terms
-//// of the MIT license.  See the LICENSE file for details.
-//// </license>
-//#endregion
-//namespace Core.Tests
-//{
-//    using System;
+﻿#region Header
+// <copyright company="Joshua Moon" file="LoggerTests.cs">
+// Copyright (c) 2015 Joshua Moon All Rights Reserved.
+// </copyright>
+// <summary>
+// LoggerTests.cs is a part of the project Core.Tests. 
+// </summary>
+// <author>Joshua Moon</author>
+// <license>
+// This software may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+// </license>
+#endregion
+namespace Core.Tests
+{
+    using System;
 
-//    using log4net;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-//    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
 
-//    using Moq;
+    using Ninject.Extensions.Logging;
 
-//    [TestClass]
-//    public class LoggerTests
-//    {
-//        private Logger _logger;
+    [TestClass]
+    public class LoggerTests
+    {
+        private Logger _logger;
 
-//        private Mock<ILog> _mock;
+        private Mock<ILogger> _mock;
 
-//        [TestInitialize]
-//        public void Init()
-//        {
-//            this._mock = new Mock<ILog>();
-//            this._logger = new Logger(this._mock.Object);
-//        }
+        [TestInitialize]
+        public void Init()
+        {
+            this._mock = new Mock<ILogger>();
+            this._logger = new Logger(this._mock.Object);
+        }
 
-//        [TestCleanup]
-//        public void Cleanup()
-//        {
-//            this._mock = null;
-//            this._logger = null;
-//        }
+        [TestCleanup]
+        public void Cleanup()
+        {
+            this._mock = null;
+            this._logger = null;
+        }
 
-//        [TestMethod]
-//        public void Warn_TakesMessageAndCallsMock_ShouldPassMessageToMock()
-//        {
-//            var message = "Warning Message";
+        [TestMethod]
+        public void Warn_TakesMessageAndCallsMock_ShouldPassMessageToMock()
+        {
+            var message = "Warning Message";
 
-//            this._logger.Warn(message);
+            this._logger.Warn(message);
 
-//            this._mock.Verify(m => m.Warn(message), Times.Once);
-//        }
+            this._mock.Verify(m => m.Warn(message), Times.Once);
+        }
 
-//        [TestMethod]
-//        public void Warn_TakesMessageAndExceptionCallsMock_ShouldPassBothToMock()
-//        {
-//            var message = "Warning message!";
-//            var exception = new SystemException(message);
+        [TestMethod]
+        public void Warn_TakesMessageAndExceptionCallsMock_ShouldPassBothToMock()
+        {
+            var message = "Warning message!";
+            var exception = new SystemException(message);
 
-//            this._logger.Warn(message, exception);
+            this._logger.Warn(message, exception);
 
-//            this._mock.Verify(m => m.Warn(message, exception), Times.Once);
-//        }
-//    }
-//}
+            this._mock.Verify(m => m.Warn(message, exception), Times.Once);
+        }
+    }
+}
